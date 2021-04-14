2604b9b89da9f8ee021f92a3255a1d92e42dd065
nopeitagain@mail.ru <nopeitagain@mail.ru>
2021-04-14 18:29:53 +0300

    RandomNumber Guessing

--- CAPP1/CAPP1.cpp
+++ CAPP1/CAPP1.cpp
@@ -4,12 +4,35 @@ using namespace std;
 
 int main()
 {
-	int x = 0;
 	setlocale(LC_ALL, "Russian");
-	for (;;)
+	srand(time(0));
+	int resultnum = rand()%10+1;
+	int usernum;
+	for (int n = 0; n <= 15; n++)
 	{
-		cout << x << endl;
-		x++;
+		cout << "Попробуй угадать мое число!: ";
+		cin >> usernum;
+		if (usernum == resultnum)
+		{
+			cout << "Это оно!" << endl;
+			break;
+		}
+		else
+		{
+			cout << "Нет, это не " << usernum << endl;
+			if (usernum > resultnum)
+			{
+				cout << "Нужное число меньше чем " << usernum << endl;
+			}
+			if (usernum < resultnum)
+			{
+				cout << "Нужное число больше чем " << usernum << endl;
+			}
+		}
+		if (n == 15)
+		{
+			cout << "У тебя закончились попытки! Ты проиграл.";
+		}
 	}
 	return 0;
 }
